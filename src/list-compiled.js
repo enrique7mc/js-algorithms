6bad2edb740c66508953d97e9cfa9b5fa6f5141e
enrique.munguia <enrique.munguia@hulu.com>
2016-07-11 17:26:07 -0500

    add methods to List

--- src/list-compiled.js
+++ src/list-compiled.js
@@ -4,8 +4,40 @@ function List() {
     this.dataStore = [];
 }
 
+List.prototype.clear = function () {
+    this.dataStore = [];
+    this.listSize = this.position = 0;
+};
+
 List.prototype.append = function (element) {
     this.dataStore[this.listSize++] = element;
+};
+
+List.prototype.toString = function () {
+    return `[${ this.dataStore.join(', ') }]`;
+};
+
+List.prototype.getElement = function () {
+    return this.dataStore[this.position];
+};
+
+List.prototype.find = function (element) {
+    for (var i = 0; i < this.listSize; i++) {
+        if (this.dataStore[i] === element) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+List.prototype.insert = function (element, after) {
+    var afterIndex = this.find(after);
+    if (afterIndex === -1) {
+        return false;
+    }
+    this.dataStore.splice(afterIndex + 1, 0, element);
+    this.listSize++;
+    return true;
 };
 
 module.exports = List;
--- test/list.test.js
+++ test/list.test.js
@@ -23,12 +23,69 @@ describe('List', function () {
         });
     });
 
+    describe('clear(e)', function () {
+        it('should clear the list', function () {
+            let list = new List();
+            list.append(1);
+            list.append(2);
+            expect(list.listSize).toBe(2);
+            list.clear();
+            expect(list.listSize).toBe(0);
+            expect(list.position).toBe(0);
+        });
+    });
+
+    describe('toString()', function () {
+        it('should return the list as string', function () {
+            let list = new List();
+            expect(list.toString()).toBe('[]');
+            list.append(1);
+            list.append(2);
+            list.append(3);
+            expect(list.toString()).toBe('[1, 2, 3]');
+        });
+    });
+
     describe('append(e)', function () {
         it('should append an element at next position', function () {
             let list = new List();
             list.append(1);
             list.append(2);
             expect(list.listSize).toBe(2);
+        });
+    });
+
+    describe('getElement()', function () {
+        it('should getElement at current position', function () {
+            let list = new List();
+            expect(list.getElement()).toBe(undefined);
+            list.append(1);
+            list.append(2);
+            expect(list.getElement()).toBe(1);
+        });
+    });
+
+    describe('find(element)', function () {
+        it('should find element position', function () {
+            let list = new List();
+            list.append(1);
+            list.append(2);
+            list.append(3);
+            expect(list.find(3)).toBe(2);
+            expect(list.find(2)).toBe(1);
+        });
+    });
+
+    describe('insert(element, after)', function () {
+        it('should find element position', function () {
+            let list = new List();
+            list.append(1);
+            list.append(2);
+            list.append(3);
+            var result = list.insert(5, 2);
+            expect(result).toBe(true);
+            expect(list.listSize).toBe(4);
+            expect(list.toString()).toBe('[1, 2, 5, 3]');
         });
     });
 });
