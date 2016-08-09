800c25d2fe0cba1f0fe00b966c041d8dd4289a4f
enrique.munguia <enrique.munguia@hulu.com>
2016-08-08 22:27:51 -0500

    add eslint

--- src/tree.js
+++ src/tree.js
@@ -1,138 +1,138 @@
 'use strict';
 
-function Node (data, left, right) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
+function Node(data, left, right) {
+  this.data = data;
+  this.left = left;
+  this.right = right;
 }
 
-function BST () {
-    this.root = null;
-    this.insert = insert;
-    this.getMin = getMin;
-    this.getMax = getMax;
-    this.find = find;
+function BST() {
+  this.root = null;
+  this.insert = insert;
+  this.getMin = getMin;
+  this.getMax = getMax;
+  this.find = find;
 }
 
-function insert (data) {
-    let n = new Node(data, null, null);
-    if (this.root == null) {
-        this.root = n;
-        return;
+function insert(data) {
+  const n = new Node(data, null, null);
+  if (this.root == null) {
+    this.root = n;
+    return;
+  }
+
+  let current = this.root;
+  let parent;
+  while (true) {
+    parent = current;
+    if (data < current.data) {
+      current = current.left;
+      if (current == null) {
+        parent.left = n;
+        break;
+      }
+    } else {
+      current = current.right;
+      if (current == null) {
+        parent.right = n;
+        break;
+      }
+    }
+  }
+}
+
+function inOrder(node) {
+  if (node == null) return;
+  inOrder(node.left);
+  console.log(node.data);
+  inOrder(node.right);
+}
+
+function preOrder(node) {
+  if (node == null) return;
+  console.log(node.data);
+  preOrder(node.left);
+  preOrder(node.right);
+}
+
+function posOrder(node) {
+  if (node == null) return;
+  posOrder(node.left);
+  posOrder(node.right);
+  console.log(node.data);
+}
+
+function getMin() {
+  let current = this.root;
+  while (current.left != null) {
+    current = current.left;
+  }
+  return current.data;
+}
+
+function getMax() {
+  let current = this.root;
+  while (current.right != null) {
+    current = current.right;
+  }
+  return current.data;
+}
+
+function find(data) {
+  let current = this.root;
+
+  while (current != null) {
+    if (current.data === data) {
+      return current;
     }
 
-    let current = this.root;
-    let parent;
-    while (true) {
-        parent = current;
-        if (data < current.data) {
-            current = current.left;
-            if (current == null) {
-                parent.left = n;
-                break;
-            }
-        } else {
-            current = current.right;
-            if (current == null) {
-                parent.right = n;
-                break;
-            }
-        }
+    if (data < current.data) {
+      current = current.left;
+    } else {
+      current = current.right;
     }
+  }
 }
 
-function inOrder (node) {
-    if(node == null) return;
-    inOrder(node.left);
-    console.log(node.data);
-    inOrder(node.right);
-}
-
-function preOrder (node) {
-    if(node == null) return;
-    console.log(node.data);
-    preOrder(node.left);
-    preOrder(node.right);
-}
-
-function posOrder (node) {
-    if(node == null) return;
-    posOrder(node.left);
-    posOrder(node.right);
-    console.log(node.data);
-}
-
-function getMin () {
-    let current = this.root;
-    while(current.left != null) {
-        current = current.left;
-    }
-    return current.data;
-}
-
-function getMax () {
-    let current = this.root;
-    while(current.right != null) {
-        current = current.right;
-    }
-    return current.data;
-}
-
-function find (data) {
-    let current = this.root;
-
-    while (current != null) {
-        if (current.data === data) {
-            return current;
-        }
-
-        if (data < current.data) {
-            current = current.left;
-        } else {
-            current = current.right;
-        }
-    }
-}
-
-function remove (data) {
-    this.root = removeNode(this.root, data);
+function remove(data) {
+  this.root = removeNode(this.root, data);
 }
 
 function removeNode(node, data) {
-    if (node == null) {
-        return null;
-    }
-    if (data == node.data) {
+  if (node == null) {
+    return null;
+  }
+  if (data == node.data) {
         // node has no children
-        if (node.left == null && node.right == null) {
-            return null;
-        }
+    if (node.left == null && node.right == null) {
+      return null;
+    }
         // node has no left child
-        if (node.left == null) {
-            return node.right;
-        }
+    if (node.left == null) {
+      return node.right;
+    }
         // node has no right child
-        if (node.right == null) {
-            return node.left;
-        }
+    if (node.right == null) {
+      return node.left;
+    }
         // node has two children
-        var tempNode = getSmallest(node.right);
-        node.data = tempNode.data;
-        node.right = removeNode(node.right, tempNode.data);
-        return node;
-    }
-    else if (data < node.data) {
-        node.left = removeNode(node.left, data);
-        return node;
-    }
+    const tempNode = getSmallest(node.right);
+    node.data = tempNode.data;
+    node.right = removeNode(node.right, tempNode.data);
+    return node;
+  }
+  else if (data < node.data) {
+    node.left = removeNode(node.left, data);
+    return node;
+  }
     else {
-        node.right = removeNode(node.right, data);
-        return node;
-    }
+    node.right = removeNode(node.right, data);
+    return node;
+  }
 }
 
 
-var nums = new BST();
+const nums = new BST();
 nums.insert(23);
 nums.insert(45);
 nums.insert(16);
