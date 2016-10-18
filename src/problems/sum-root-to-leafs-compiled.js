5c33c8c75c37de61bf99725cea374c189b303f1b
enrique.munguia <enrique.munguia@hulu.com>
2016-10-18 15:53:13 -0500

    add recursive version

--- src/problems/sum-root-to-leafs-compiled.js
+++ src/problems/sum-root-to-leafs-compiled.js
@@ -15,7 +15,7 @@ function isLeaf(node) {
 function sumRootToLeafs(node) {
     if (!node) return 0;
 
-    node.list = [];
+    node.sum = [];
     let result = 0;
     const queue = [node];
     while (queue.length) {
@@ -56,6 +56,22 @@ function updateTotal(node) {
     return result;
 }
 
+function sumRootToLeafsRecursive(node) {
+    return sumHelper(node, 0);
+}
+
+function sumHelper(node, partialSum) {
+    if (!node) return 0;
+
+    partialSum = partialSum * 10 + node.data;
+
+    if (isLeaf(node)) {
+        return partialSum;
+    }
+
+    return sumHelper(node.left, partialSum) + sumHelper(node.right, partialSum);
+}
+
 let n4 = new Node(4);
 let n5 = new Node(2);
 let n6 = new Node(5);
@@ -64,7 +80,7 @@ let n3 = new Node(3, n5, n6);
 let n2 = new Node(2, n4);
 let n1 = new Node(1, n2, n3);
 
-const result = sumRootToLeafs(n1);
+const result = sumRootToLeafsRecursive(n1);
 console.log(result);
 
 //# sourceMappingURL=sum-root-to-leafs-compiled.js.map
